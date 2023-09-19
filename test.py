31a8063a2096532b75df06c61205e9e7d3d6f865
Богдан Нугманов <bogdan0957@yandex.ru>
2023-09-19 17:48:31 +0400

    Add new files games

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
 from math import sqrt
 
-message = 'Добро пожаловать в самую лучшую программу для вычисления '
-' квадратного корня из заданного числа'
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
 print(message)
 
 
-def calculatesquareroot(number):
-    """ Вычисляет квадратный корень"""
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
     return sqrt(number)
 
 
 def calc(your_number):
+    """Описываем функцию."""
     if your_number <= 0:
         return
+    my_number = calculate_square_root(your_number)
+    print(f'Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет: {my_number}')
 
-        print(f'Мы вычислили квадратный корень из введённого вами числа. '
-              f'Это будет: {calculatesquareroot(your_number)}')
-
-
-print(message)
 
 calc(25.5)
